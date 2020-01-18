264f207e7694ddc1f85524f9701fe359ddcb2fdf
Nate Hapke <31361826+shadedblue@users.noreply.github.com>
2020-01-17 18:41:48 -0800

    15: Party everyday
    Task-Url: http://github.com/shadedblue/campingbot/issues/15

--- ca.hapke.campbinning.bot/src/ca/hapke/campbinning/bot/commands/PartyEverydayCommand.java
+++ ca.hapke.campbinning.bot/src/ca/hapke/campbinning/bot/commands/PartyEverydayCommand.java
@@ -1,5 +1,10 @@
 package ca.hapke.campbinning.bot.commands;
 
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
@@ -12,6 +17,8 @@ import ca.hapke.campbinning.bot.CampingBot;
  */
 public class PartyEverydayCommand extends RespondWithImage {
 
+	private final ZoneId zone = ZoneId.systemDefault();
+
 	private static final String[] urls = new String[] { "http://www.hapke.ca/images/party-boy1.gif",
 			"http://www.hapke.ca/images/party-boy2.gif", "http://www.hapke.ca/images/party-boy3.gif" };
 	private static final int[] imageTypes = new int[] { GIF, GIF, GIF };
@@ -22,6 +29,16 @@ public class PartyEverydayCommand extends RespondWithImage {
 
 	@Override
 	public boolean isMatch(String msg, List<MessageEntity> entities) {
+		Instant now = Instant.now();
+		LocalDate ld = LocalDate.ofInstant(now, zone);
+		LocalTime lt = LocalTime.ofInstant(now, zone);
+		int day = ld.getDayOfWeek().getValue();
+		int hours = lt.getHour();
+		if (day >= DayOfWeek.MONDAY.getValue() && day <= DayOfWeek.FRIDAY.getValue() && hours >= 8 && hours <= 16) {
+			// weekdays SFW
+			return false;
+		}
+
 		String lowerCase = msg.toLowerCase();
 		boolean matches = lowerCase.matches(".*pa[r]+ty everyday.*");
 		return matches;
